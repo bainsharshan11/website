eb24cbc0acf43db4a3019330dff3c5b8e439676e
bainsharshan11 <hagsbains@gmail.com>
2026-02-09 07:45:38 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,42 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import CityPage from "./pages/CityPage";
 
-function App() {
+import Navigation from "./components/Navigation";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Services from "./components/Services";
+import Process from "./components/Process";
+import Safety from "./components/Safety";
+import Gallery from "./components/Gallery";
+import Testimonials from "./components/Testimonials";
+import FAQ from "./components/FAQ";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+import City from "./pages/City";
+
+function Home() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/:city" element={<CityPage />} />
-    </Routes>
+    <div className="min-h-screen">
+      <Navigation />
+      <Hero />
+      <About />
+      <Services />
+      <Process />
+      <Safety />
+      <Gallery />
+      <Testimonials />
+      <FAQ />
+      <Contact />
+      <Footer />
+    </div>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/:city" element={<City />} />
+    </Routes>
+  );
+}
