a3c728a93deccf035b67ced365c04407e83931f6
bainsharshan11 <hagsbains@gmail.com>
2026-02-09 04:00:54 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,13 @@
-import Navigation from './components/Navigation';
-import Hero from './components/Hero';
-import About from './components/About';
-import Services from './components/Services';
-import Process from './components/Process';
-import Safety from './components/Safety';
-import Gallery from './components/Gallery';
-import Testimonials from './components/Testimonials';
-import FAQ from './components/FAQ';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import CityPage from "./pages/CityPage";
 
 function App() {
   return (
-    <div className="min-h-screen">
-      <Navigation />
-      <Hero />
-      <About />
-      <Services />
-      <Process />
-      <Safety />
-      <Gallery />
-      <Testimonials />
-      <FAQ />
-      <Contact />
-      <Footer />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/:city" element={<CityPage />} />
+    </Routes>
   );
 }
 
